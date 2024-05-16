680e96afbdeabffd183e9380164a94ceb16e60cd
Costin-Ioana-Alexandra <ioana-alexandra.costin@student.tuiasi.ro>
2024-05-17 00:46:28 +0300

    modified gui and main_modular

--- main_modular.m
+++ main_modular.m
@@ -15,54 +15,56 @@ addpath("dataset\");
 % imshow(image);
 
 % Matrice de test
-matrice = [3, 0, 6, 5, 0, 8, 4, 0, 0;
-           5, 2, 0, 0, 0, 0, 0, 0, 0;
-           0, 8, 7, 0, 0, 0, 0, 3, 1;
-           0, 0, 3, 0, 1, 0, 0, 8, 0;
-           9, 0, 0, 8, 6, 3, 0, 0, 5;
-           0, 5, 0, 0, 9, 0, 6, 0, 0;
-           1, 3, 0, 0, 0, 0, 2, 5, 0;
-           0, 0, 0, 0, 0, 0, 0, 7, 4;
-           0, 0, 5, 2, 0, 6, 3, 0, 0];
+matrice = [
+           0, 5, 1, 0, 0, 0, 0, 7, 0;
+           0, 7, 3, 0, 4, 2, 8, 9, 5;
+           9, 4, 2, 5, 8, 0, 6, 0, 3;
+           0, 6, 9, 7, 1, 0, 3, 0, 0;
+           0, 0, 0, 8, 2, 9, 7, 4, 6;
+           2, 8, 0, 0, 6, 0, 0, 0, 1;
+           3, 0, 6, 0, 0, 8, 0, 0, 9;
+           7, 0, 4, 2, 5, 6, 1, 3, 8;
+           5, 2, 8, 9, 0, 0, 4, 0, 7
+                ];
 
-image = imread("dataset\easy_3.png");
+% image = imread("dataset\easy_3.png");
+% 
+% colturi=find_closest_corners(image);
+% 
+% doarS=crop_sudoku_grid(image, colturi);
+% 
+% inputOCR=extract_sudoku_cells(doarS);
+% 
+% folderPath="input_OCR\";
 
-colturi=find_closest_corners(image);
+% files = dir(fullfile(folderPath, '*'));
+% for i = 1:length(files)
+%     if ~files(i).isdir
+%         delete(fullfile(folderPath, files(i).name));
+%     end
+% end
 
-doarS=crop_sudoku_grid(image, colturi);
+% % Iterate over subimages and save them
+% for i = 1:9
+%     for j = 1:9
+%         % Generate filename
+%         filename = sprintf('subimage_%d_%d.png', i, j);
+% 
+%         % Construct full file path
+%         fullFilePath = fullfile(folderPath, filename);
+% 
+%         % Save subimage
+%         imwrite(inputOCR{i, j}, fullFilePath);
+%     end
+% end
+% 
+% 
+% matrice=folderRead(folderPath);
 
-inputOCR=extract_sudoku_cells(doarS);
-
-folderPath="input_OCR\";
-
-files = dir(fullfile(folderPath, '*'));
-for i = 1:length(files)
-    if ~files(i).isdir
-        delete(fullfile(folderPath, files(i).name));
-    end
-end
-
-% Iterate over subimages and save them
-for i = 1:9
-    for j = 1:9
-        % Generate filename
-        filename = sprintf('subimage_%d_%d.png', i, j);
-        
-        % Construct full file path
-        fullFilePath = fullfile(folderPath, filename);
-        
-        % Save subimage
-        imwrite(inputOCR{i, j}, fullFilePath);
-    end
-end
-
-
-matrice=folderRead(folderPath);
-
-solved_matrix = solveSudoku(matrice');
+solved_matrix = solveSudoku(matrice);
 
 disp("Intrare: ");
-disp(matrice');
+disp(matrice);
 disp("Iesire");
 disp(solved_matrix);
 
